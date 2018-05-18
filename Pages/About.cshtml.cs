5decc88aa41994202a1290b799d69e4b8efdf8d7
tjrocha <jrocha@usbr.gov>
2018-05-18 10:19:42 -0700

    initial commit. need to wire app to API.

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "This web application is being developed internally via a technology stack comprised of C#, .NetCore, and the HDB API. "
+                + "Contact the developer shown below for additional information. ";
+        }
+    }
+}
---
+++ wwwroot/scripts/_layout.js
@@ -0,0 +1,13 @@
+﻿/** add active class and stay opened when selected */
+var url = window.location;
+
+// SET ACTIVE SIDEBAR ITEM
+// for sidebar menu entirely but not cover treeview
+$('ul.sidebar-menu a').filter(function () {
+    return this.href == url;
+}).parent().addClass('active');
+
+// for treeview
+$('ul.treeview-menu a').filter(function () {
+    return this.href == url;
+}).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
