6bde8721f996ec8ee4fd25a8a44df525af76004f
tjrocha <jrocha@usbr.gov>
2018-05-21 12:15:44 -0700

    allow app to authenticate against HDB using the HDB API

---
+++ wwwroot/scripts/_SeriesSelection.js
@@ -0,0 +1,43 @@
+﻿//Date range picker with time picker
+$('#input-date-times').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
+
+//Date range as a button
+$('#daterange-btn').daterangepicker(
+    {
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        startDate: moment().subtract(29, 'days'),
+        endDate: moment()
+    },
+    function (start, end) {
+        $('#input-date-times').te.html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+    }
+)
+function toggleDataType() {
+    var rDisabled = document.getElementById('optionsRadios1').checked;
+    if (rDisabled) {
+        $("#modelrunid-input").prop("disabled", true);
+        $("#modelid-dropdown").prop("disabled", true);
+        $("#modelrunid-dropdown").prop("disabled", true);
+    }
+    else {
+        $("#modelrunid-input").prop("disabled", false);
+        $("#modelid-dropdown").prop("disabled", false);
+        $("#modelrunid-dropdown").prop("disabled", false);
+    }
+}
+function selectAllObjectTypes() {
+    var allChecked = document.getElementById('object-types-select-all').checked;
+    if (allChecked) {
+        $('#object-types-dropdown option').prop('selected', true);
+    }
+    else {
+        $('#object-types-dropdown option').prop('selected', false);
+    }
+}
--- wwwroot/scripts/_layout.js
+++ wwwroot/scripts/_layout.js
@@ -1,5 +1,6 @@
 ﻿/** add active class and stay opened when selected */
 var url = window.location;
+var hdbApiUrl = 'http://ibr3lcrsrv02.bor.doi.net';
 
 // SET ACTIVE SIDEBAR ITEM
 // for sidebar menu entirely but not cover treeview
@@ -11,3 +12,45 @@ $('ul.sidebar-menu a').filter(function () {
 $('ul.treeview-menu a').filter(function () {
     return this.href == url;
 }).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
+
+// for log-in
+function hdbConnect() {
+    var selectedHdb = document.getElementById('logInHDB').value
+    var selectedUser = document.getElementById('logInUser').value
+    var selectedPass = document.getElementById('logInPass').value
+    var guestLogIn = document.getElementById('guest-login-checkbox').checked
+    if (guestLogIn) {
+        selectedUser = 'app_user';
+        selectedPass = selectedHdb;
+    }
+    $.ajax({
+        type: "GET",
+        url: hdbApiUrl + "/connect?"
+        + 'hdb=' + selectedHdb
+        + '&username=' + selectedUser
+        + '&password=' + selectedPass,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function OnSuccess(data, response) {
+            //console.log(data);
+            //var resp = JSON.parse(data);
+            document.getElementById("login-text-helper").textContent = response + ' | ' + data[0].username.toLowerCase();
+            document.getElementById("connected-hdb-user").textContent = data[0].username.toLowerCase();
+            document.getElementById("connected-hdb-name").textContent = selectedHdb;
+            document.getElementById("connected-hdb-status").textContent = ' Online';
+            //alert(response + ' | ' + data[0].username);
+        },
+        failure: function (response) {
+            document.getElementById("login-text-helper").textContent = 'Log-In failed, try again later...';
+            //alert('Log-In failed, try again later...');
+        },
+        error: function (xhr, statusText, err, response) {
+            var resp = JSON.parse(xhr.responseText);
+            document.getElementById("login-text-helper").textContent = 'Error: ' + resp.message;
+            document.getElementById("connected-hdb-user").textContent = 'Guest';
+            document.getElementById("connected-hdb-name").textContent = 'XXHDBX';
+            document.getElementById("connected-hdb-status").textContent = ' Offline';
+            //alert("Error: " + resp.message);
+        }
+    });
+}
